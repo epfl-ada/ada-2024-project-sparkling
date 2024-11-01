80d9bbbcc2aa82d5fe0e3be7f937e8db87d59f4b
Matthias Wyss <matthiaswyss74@gmail.com>
2024-11-01 12:16:23 +0100

    code that produced csv

--- src/data/emotions.py
+++ src/data/emotions.py
@@ -1,9 +1,14 @@
 import pandas as pd
 from transformers import pipeline
 from tqdm import tqdm
+import re
 
 # Predicts the emotions present in a given movie plot by using a pre-trained classifier.
 def predict_emotions_from_plot(classifier, plot: str):
+    plot = plot.strip()
+    plot = re.sub(r'<.*?>', '', plot)
+    plot = plot.replace('&ndash;', '–')
+    
     predictions = {
         'anger': 0,
         'disgust': 0,
@@ -14,12 +19,23 @@ def predict_emotions_from_plot(classifier, plot: str):
         'surprise': 0
     }
     
+    if plot.strip() == "":
+        return {label: None for label in predictions}
+    
     sentences = plot.split('.')
     if sentences[-1].strip() == "":
         sentences = sentences[:-1]
     
     for s in sentences:
-        results = classifier(s.strip())[0]
+        while len(s) > 1500:
+            sentences.append(s[1500:])
+            s = s[:1500]
+        s = s.strip()
+        results = classifier(s)
+        if not results:
+            continue
+            
+        results = results[0]
         
         for result in results:
             label = result['label']
@@ -27,16 +43,31 @@ def predict_emotions_from_plot(classifier, plot: str):
             predictions[label] += score
             
     total_score = sum(predictions.values())
-    normalized_predictions = {label: score / total_score for label, score in predictions.items()}
+    
+    if total_score == 0:
+        normalized_predictions = {label: None for label in predictions}
+    else:
+        normalized_predictions = {label: score / total_score for label, score in predictions.items()}
     
     return normalized_predictions
 
-# Adds emotion prediction columns to a DataFrame of movies based on their plots.
-def add_emotions_to_df(df_movies):
+
+
+# Adds emotion prediction columns to a DataFrame of movies based on their plots and save them into a csv.
+def predict_emotions_to_csv(df_movies, csv_path='emotions.csv'):
     classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", top_k=None)
     tqdm.pandas(desc="Processing emotions")
     emotion_predictions = df_movies['plot'].progress_apply(lambda plot: predict_emotions_from_plot(classifier, plot))
     df_emotion = pd.DataFrame(emotion_predictions.tolist())
     df_movies_with_emotions = pd.concat([df_movies, df_emotion], axis=1)
     
-    return df_movies_with_emotions
+    df_movies_with_emotions.to_csv(csv_path,
+                               columns=['wikipedia_ID', 'anger', 'disgust', 'fear', 'joy', 'neutral', 'sadness', 'surprise'], 
+                               index=False)
+
+
+# Function to merge the emotion predictions CSV with df_movies on wikipedia_ID
+def merge_df_movies_with_emotions_csv(df_movies, csv_path='emotions.csv'):
+    df_emotions = pd.read_csv(csv_path)
+    merged_df = pd.merge(df_movies, df_emotions, on='wikipedia_ID')
+    return merged_df
